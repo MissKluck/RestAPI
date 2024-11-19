258b2cb14696beac4a236911d1a47813a81968c9
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-19 12:35:07 +0100

    added comments

--- Model/Movies.cs
+++ Model/Movies.cs
@@ -1,5 +1,12 @@
 public class Movies
 {
+    /// <summary>
+    /// Our Movies endpoint model. It contains the following fields
+    /// int Id
+    /// string Title
+    /// string Director
+    /// int ReleaseYear
+    /// </summary>/
     public required int Id { get; set; }
     public required string Title { get; set; }
     public required string Director { get; set; }
--- Model/TVShow.cs
+++ Model/TVShow.cs
@@ -1,5 +1,13 @@
 public class TVShows
 {
+    /// <summary>
+    /// Our TVShow endpoint model. It contains the following fields
+    /// int Id
+    /// string Title
+    /// string Creators
+    /// int ReleaseYear
+    /// int NumberOfSeasons
+    /// </summary>/
     public required int Id { get; set; }
     public required string Title { get; set; }
     public required string Creators { get; set; }

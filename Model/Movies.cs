1be90956b8ca3a3375d66127b677fea7739c8f8a
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-19 12:01:07 +0100

    created models and filled in data from lecture

---
+++ Model/Movies.cs
@@ -0,0 +1,8 @@
+public class Movies
+{
+    public required int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Director { get; set; }
+    public required int ReleaseYear { get; set; }
+
+}
---
+++ Model/TVShow.cs
@@ -0,0 +1,9 @@
+public class TVShows
+{
+    public required int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Creators { get; set; }
+    public required int ReleaseYear { get; set; }
+    public required int NumberOfSeasons { get; set; }
+
+}
